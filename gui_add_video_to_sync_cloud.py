4172c3e1afa9d72510511f0912e0b811b7e53259
zyf4 <zyf4@meitu.com>
2023-02-15 22:29:32 +0800

    [update] export to sync

---
+++ gui_add_video_to_sync_cloud.py
@@ -0,0 +1,126 @@
+import os
+
+import tkinter as tk
+from tkinter import messagebox as msg
+from tkinter import filedialog
+from tkinter import *
+from moviepy.editor import VideoFileClip, AudioFileClip, afx
+from data_util import *
+import json
+import shutil
+import time
+
+class AddVideo(Toplevel):
+    def __init__(self, master=None):
+        super().__init__(master=master)
+
+        self.title("添加视频到同步云")
+        self.geometry("1000x700")
+        self.resizable(False, False)
+
+        self.standard_font = (None, 16)
+
+        self.main_frame = tk.Frame(self, width=1000, height=600)
+        self.main_frame.grid(row=5, column=4)
+
+        self.default_source = r'/Users/meitu/Documents/midlife_crisis/project/dy_project/data/素材库/download_video'
+
+        self.video_str = tk.StringVar(value=self.default_source)
+        tk.Label(self.main_frame, text="视频路径:").grid(row=0, column=0)
+        tk.Entry(self.main_frame, textvariable=self.video_str, width=80).grid(row=0, column=1, columnspan=2, sticky=NSEW)
+        tk.Button(self.main_frame, text="选择", command=self.select_source_path, width=10).grid(row=0, column=3)
+
+        # self.dst_path = tk.StringVar(value=self.default_save)
+        # tk.Label(self.main_frame, text="保存路径:").grid(row=1, column=0)
+        # tk.Entry(self.main_frame, textvariable=self.source_path, width=30).grid(row=1, column=1, rowspan=2)
+        # tk.Button(self.main_frame, text="选择", command=self.select_source_path, width=10).grid(row=1, column=3)
+
+        tk.Label(self.main_frame, text="导出路径:").grid(row=1, column=0, rowspan=2)
+        tk.Label(self.main_frame, text="快手设备").grid(row=1, column=1)
+        tk.Label(self.main_frame, text="抖音设备").grid(row=1, column=2)
+
+        self.ks_dir = "/Users/meitu/同步空间/KS"
+        ks_device_ids = [dirname for dirname in os.listdir(self.ks_dir) if os.path.isdir(os.path.join(self.ks_dir, dirname))]
+        self.ks_devices_items = tk.StringVar(value=ks_device_ids)
+        self.ks_device_list_box = tk.Listbox(self.main_frame, cursor='arrow', selectborderwidth=2, listvariable=self.ks_devices_items,
+                                          selectmode=MULTIPLE)
+        self.ks_device_list_box.grid(row=2, column=1, sticky=NSEW, padx=10)
+        self.ks_device_list_box.configure(exportselection=False)
+
+        self.dy_dir = "/Users/meitu/同步空间/DY"
+        dy_device_ids = [dirname for dirname in os.listdir(self.dy_dir) if os.path.isdir(os.path.join(self.dy_dir, dirname))]
+        self.dy_devices_items = tk.StringVar(value=dy_device_ids)
+        self.dy_device_list_box = tk.Listbox(self.main_frame, cursor='arrow', selectborderwidth=2, listvariable=self.dy_devices_items,
+                                          selectmode=MULTIPLE)
+        self.dy_device_list_box.grid(row=2, column=2, sticky=NSEW, padx=10)
+        self.dy_device_list_box.configure(exportselection=False)
+
+
+        self.koulin = tk.StringVar()
+        tk.Label(self.main_frame, text="口令:").grid(row=3, column=0)
+        tk.Entry(self.main_frame, textvariable=self.koulin).grid(row=3, column=1, columnspan=2, sticky=NSEW)
+        tk.Button(self.main_frame, text="保存", command=self.save, width=10).grid(row=3, column=3)
+
+        self.main_frame.pack(fill=tk.BOTH, expand=1)
+
+    #
+    # def select_save_path(self):
+    #     save_path = tk.filedialog.askdirectory(initialdir=self.default_save)
+    #     if save_path is None:
+    #         return
+    #     self.save_path.set(save_path)
+
+    def select_source_path(self):
+        self.video_pathes = tk.filedialog.askopenfiles(filetypes=[("Configuration file", "*.mp4")],
+                                                  initialdir=self.default_source)
+        self.video_str.set(self.video_pathes)
+
+        print(self.video_pathes)
+
+
+    def get_box_list(self, list_box, parent_dir):
+        select_indexes = list_box.curselection()
+        box_list = []
+        for index in select_indexes:
+            box_list.append(list_box.get(index))
+
+        for i in range(len(box_list)):
+            box_list[i] = os.path.join(parent_dir, box_list[i])
+
+        return box_list
+
+    def save(self):
+
+        if len(self.video_pathes) == 0:
+            msg.showinfo("状态", f"请先选择路径！")
+            return
+
+        save_dir = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+
+        save_paths = self.get_box_list(self.dy_device_list_box, self.dy_dir) + \
+                     self.get_box_list(self.ks_device_list_box, self.ks_dir)
+
+        if len(save_paths) == 0:
+            msg.showinfo("状态", f"请先选择导出设备！")
+            return
+
+        koulin = self.koulin.get()
+        try:
+            koulin = int(koulin)
+            koulin = f"口令{koulin}"
+        except:
+            koulin = ""
+        for i, filepath in enumerate(self.video_pathes):
+            filepath = filepath.name
+            if str(filepath).endswith('.mp4'):
+                save_path = os.path.join(save_paths[i % len(save_paths)], save_dir, str(i // len(save_paths))+"_"+koulin+os.path.basename(filepath))
+                # if not os.path.exists(os.path.dirname(save_path)):
+                #     os.makedirs(os.path.dirname(save_path))
+                print(filepath, save_path)
+                # shutil.move()
+
+        msg.showinfo("状态", f"导入成功")
+
+if __name__ == "__main__":
+    timer = AddVideo()
+    timer.mainloop()
--- scripts/export_vcf_card.py
+++ scripts/export_vcf_card.py
@@ -2,8 +2,81 @@ import os
 import pandas as pd
 import io
 import argparse
-import msoffcrypto
+# import msoffcrypto
 import pandas as pd
+
+phone_list = [15959571093,
+15659829007,
+15959572907,
+15260760100,
+15860552421,
+13665945636,
+15759717081,
+15860556623,
+19559581236,
+17302258183,
+13665933529,
+15959571790,
+15859573507,
+13055657567,
+18659556430,
+17268325603,
+15160006789,
+18659010106,
+18750552915,
+15106057883,
+15105990255,
+18160923222,
+13626977328,
+15260354868,
+13625993535,
+19905071232,
+18965850170,
+13559368788,
+15280852573,
+18250737274,
+13950198808,
+15959538421,
+13559506582,
+13489300106,
+18650428713,
+15859550079,
+13314964285,
+18659405888,
+18250620272,
+13850735137,
+13315795402,
+18016711819,
+19859570119,
+13960382981,
+13599231996,
+13459241294,
+15259589865,
+13645911500,
+13860732629,
+13850739160,
+13015873335,
+15759717837,
+15396598368,
+18396185253,
+13055658707,
+13665939386,
+13625995660,
+17729788892,
+13959933431,
+19959732909,
+18659085081,
+18759295593,
+13559030123,
+18059851080,
+15960799325,
+18859265350,
+13599733183,
+13505033839,
+15805999972,
+13004804736,
+13067076006,
+18159271738]
 
 """
 用于导出iphone cvf card
@@ -28,7 +101,25 @@ def decode_excel(file_path, password):
         return file_path
         print(f"decode {file_path} happen except!")
 
+def list_to_vcf():
+    out_path = "/Users/meitu/Documents/midlife_crisis/project/dy_project/data/素材库/"
+
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+
+    out = open(f"{out_path}/lyqt.vcf", "w")
+    for i in range(len(phone_list)):
+        print(i)
+        name = f"啊{i}{i}"
+        phone = str(phone_list[i])
+        out.write(f"BEGIN:VCARD\nVERSION:3.0\nN:{name[0]};{name[1:3]};;;\nFN:{name[1:3]} {name[0]}\nTEL;TYPE=CELL;TYPE=pref;TYPE=VOICE:{phone[0:3]} {phone[3:7]} {phone[7:11]}\nPRODID:-//Apple Inc.//iCloud Web Address Book 2220B25//EN\nREV:2022-08-22T08:45:49Z\nEND:VCARD\n")
+    out.close()
+
+
 if __name__ == "__main__":
+    list_to_vcf()
+    exit(0)
+
     save_path = "/Users/meitu/Downloads/0404上午MB.xlsx"
     excel_path = "/Users/meitu/Documents/midlife_crisis/廖荣寿/SD登记表/其它/1店11月订单_decode.xlsx"
     out_path = "/Users/meitu/Documents/midlife_crisis/project/phont_vcf/"
